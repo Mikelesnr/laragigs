ef826e1a38bacfdb04e94a4c75f240b53b077594
Mikelesnr <mwanza.n.m@gmail.com>
2022-10-19 10:31:45 +0200

    styling and parcials

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,32 +2,10 @@
 
 namespace App\Models;
 
-class Listing
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae metus ut massa mattis posuere sit amet non quam. Praesent est lacus, sagittis vel ultricies vitae, congue sit amet tortor. '
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing two',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae metus ut massa mattis posuere sit amet non quam. Praesent est lacus, sagittis vel ultricies vitae, congue sit amet tortor. '
-            ]
-        ];
-    }
-
-    public static function find($id)
-    {
-        $listings = self::all();
-
-        foreach ($listings as $listing) {
-            if ($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
-};
+    use HasFactory;
+}
